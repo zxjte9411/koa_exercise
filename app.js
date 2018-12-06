0b4150ceb669681f91b189d8320314c7fde44651
zxjte9411 <a5852241@gmail.com>
2018-12-07 00:10:38 +0800

    引入所需模組

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-const koa = require('koa')
+const Koa = require('koa')
+const logger = require('koa-logger');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const views = require('koa-views');
+const static = require('koa-static')
+
+const path = require('path');
+
+const app = new Koa();
+const router = Router();
